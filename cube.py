947226adb76f57e0178f090ee8aa4708bfef1bf2
Gimmy <gmy77@users.noreply.github.com>
2024-03-07 12:16:14 +0100

    Same project
    Two different methods.

---
+++ cube.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+class Cube:
+    def __init__(self, center, size):
+        self.center = center
+        self.size = size
+
+    def vertices(self):
+        """Calcola i vertici del cubo basati sul centro e sulla dimensione."""
+        d = self.size / 2
+        cx, cy, cz = self.center
+        return [
+            (cx - d, cy - d, cz - d), (cx + d, cy - d, cz - d),
+            (cx + d, cy + d, cz - d), (cx - d, cy + d, cz - d),
+            (cx - d, cy - d, cz + d), (cx + d, cy - d, cz + d),
+            (cx + d, cy + d, cz + d), (cx - d, cy + d, cz + d),
+        ]
+
+    def get_reflections(self, room_size):
+        """Calcola le riflessioni del cubo sugli assi x, y, z."""
+        reflections = []
+        for axis in range(3):
+            reflected = list(self.center)
+            reflected[axis] = room_size[axis] - reflected[axis]
+            reflections.append(Cube(tuple(reflected), self.size))
+        return reflections
+
+def plot_cube(ax, cube, color='blue', alpha=0.5):
+    """Disegna un cubo su un asse Matplotlib."""
+    verts = cube.vertices()
+    # Lista dei lati del cubo
+    faces = [[verts[j] for j in [0, 1, 2, 3]], [verts[j] for j in [4, 5, 6, 7]], 
+             [verts[j] for j in [0, 3, 7, 4]], [verts[j] for j in [1, 2, 6, 5]], 
+             [verts[j] for j in [0, 1, 5, 4]], [verts[j] for j in [2, 3, 7, 6]]]
+    poly3d = [Poly3DCollection(faces, color=color, linewidths=1, alpha=alpha)]
+    for poly in poly3d:
+        ax.add_collection3d(poly)
+
+# Inizializzazione della figura e degli assi
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Definizione della stanza e dei cubi
+room_size = (50, 50, 50)
+cube1 = Cube((25, 25, 5), 4)
+cube2 = Cube((25, 25, 13), 4)
+
+# Plot dei cubi
+plot_cube(ax, cube1, color='blue')
+plot_cube(ax, cube2, color='red')
+
+# Plot delle riflessioni
+for cube_reflection in cube1.get_reflections(room_size):
+    plot_cube(ax, cube_reflection, color='blue', alpha=0.2)
+
+for cube_reflection in cube2.get_reflections(room_size):
+    plot_cube(ax, cube_reflection, color='red', alpha=0.2)
+
+# Impostazione dei limiti degli assi
+ax.set_xlim(0, room_size[0])
+ax.set_ylim(0, room_size[1])
+ax.set_zlim(0, room_size[2])
+
+plt.show()
---
+++ cube2.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import numpy as np
+
+class Cube:
+    def __init__(self, center, size):
+        self.center = np.array(center)
+        self.size = size
+
+    def get_vertices(self):
+        half_size = self.size / 2
+        # Vertici del cubo rispetto al centro
+        vertices = [
+            self.center + np.array([half_size, half_size, half_size]),
+            self.center + np.array([-half_size, half_size, half_size]),
+            self.center + np.array([-half_size, -half_size, half_size]),
+            self.center + np.array([half_size, -half_size, half_size]),
+            self.center + np.array([half_size, half_size, -half_size]),
+            self.center + np.array([-half_size, half_size, -half_size]),
+            self.center + np.array([-half_size, -half_size, -half_size]),
+            self.center + np.array([half_size, -half_size, -half_size])
+        ]
+        return vertices
+
+    def get_reflection(self, axis, room_size):
+        # Riflette il cubo lungo un dato asse
+        reflection = Cube(self.center, self.size)
+        reflection.center[axis] = room_size[axis] - self.center[axis]
+        return reflection
+
+def plot_cube(vertices, ax, color='blue'):
+    # Crea le facce del cubo dalla lista dei vertici
+    edges = [
+        [vertices[0], vertices[1], vertices[2], vertices[3]],
+        [vertices[4], vertices[5], vertices[6], vertices[7]], 
+        [vertices[0], vertices[1], vertices[5], vertices[4]], 
+        [vertices[2], vertices[3], vertices[7], vertices[6]], 
+        [vertices[1], vertices[2], vertices[6], vertices[5]],
+        [vertices[4], vertices[7], vertices[3], vertices[0]]
+    ]
+    ax.add_collection3d(Poly3DCollection(edges, facecolors=color, linewidths=1, edgecolors='r', alpha=.25))
+
+# Parametri della stanza e dei cubi
+room_size = np.array([50, 50, 50])
+cube1 = Cube((25, 25, 5), 4)
+cube2 = Cube((25, 25, 13), 4)
+
+# Creazione della figura e dell'asse
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Definizione dei limiti degli assi
+ax.set_xlim(0, room_size[0])
+ax.set_ylim(0, room_size[1])
+ax.set_zlim(0, room_size[2])
+
+# Plot dei cubi originali
+plot_cube(cube1.get_vertices(), ax, 'blue')
+plot_cube(cube2.get_vertices(), ax, 'green')
+
+# Plot delle riflessioni dei cubi
+for axis in range(3):
+    reflected_cube1 = cube1.get_reflection(axis, room_size)
+    reflected_cube2 = cube2.get_reflection(axis, room_size)
+    plot_cube(reflected_cube1.get_vertices(), ax, 'lightblue')
+    plot_cube(reflected_cube2.get_vertices(), ax, 'lightgreen')
+
+plt.show()
